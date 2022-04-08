0f0119d93577c505231fde2e0ff37057b2af6efa
Ogungbamila <olugbamilaemmanuel@gmail.com>
2022-04-08 16:19:21 -0700

    My 9th task commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -21,7 +21,7 @@ for (n = 48; n < 58; n++)
 
 putchar(n);
 
-if (n != 57)
+	if (n != 57)
 
 {
 
@@ -29,7 +29,6 @@ if (n != 57)
 
 																	putchar(' ');
 																	}
-
 }
 
 putchar('\n');
